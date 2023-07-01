88a13de6e16e88296f10a4c28680ac710ec9cdd9
Melissa Longenberger <melissalongenberger24@gmail.com>
2023-07-01 19:24:22 -0400

    create and view week functions added and tested

--- logApp/urls.py
+++ logApp/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.logDash),
+    path('newWeek/', views.newWeek),
+    path('createWeek/', views.createWeek),
+    path('week/<int:week_id>/', views.viewWeek)
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- logApp/views/logs.py
+++ logApp/views/logs.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from userApp.models import *
 from logApp.models import *
+from django.core.paginator import Paginator
 
 # title = {
 #     'title': 'Index',
@@ -19,15 +20,29 @@ def logDash(request):
         messages.error(request, 'Please log in to view your logs')
         return redirect('/logReg/')
     user = User.objects.filter(id=request.session['user_id'])
+    theWeeks = Week.objects.filter(user_id=request.session['user_id']).order_by('-createdAt')
+    paginator = Paginator(theWeeks, 5)
+    pageNum = request.GET.get('page')
+    weeks = paginator.get_page(pageNum)
+    days = Day.objects.filter(author_id=request.session['user_id'])
+    print(weeks)
     print('the user', user[0])
     request.session['site'] = 'logs'
     site = request.session['site']
     context = {
         'title': title,
         'user': user[0],
-        'site': site
+        'site': site,
+        'weeks': weeks,
+        'days': days,
     }
     return render(request, 'logDash.html', context)
+
+
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
 
 def newWeek(request):
     title = {
@@ -45,3 +60,35 @@ def newWeek(request):
         'site': site
     }
     return render(request, 'createWeek.html', context)
+
+def createWeek(request):
+    newWeek = Week.objects.create(
+        title=request.POST['title'],
+        user=User.objects.get(id=request.session['user_id']),
+    )
+    newWeek = newWeek.id
+    messages.error(request, 'Week Created')
+    return redirect(f'/logs/week/{newWeek}/')
+
+def viewWeek(request, week_id):
+    week = Week.objects.get(id=week_id)
+    days = Day.objects.filter(week_id=week_id)
+    weekTitle = week.title
+    title = {
+        'title':weekTitle,
+        'header': f'Hive Mentor - {weekTitle}'
+    }
+    if 'user_id' not in request.session:
+        messages.error(request, 'Please log in to view page')
+        return redirect('/logReg/')
+    else:
+        user = User.objects.get(id=request.session['user_id'])
+        site = request.session['site']
+        context = {
+            'title': title,
+            'week': week,
+            'user': user,
+            'site': site,
+            'days': days,
+        }
+        return render(request, 'viewWeek.html', context)
