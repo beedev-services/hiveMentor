3edc42087271637be67ccf6fb229fa9db86382d4
Melissa Longenberger <melissalongenberger24@gmail.com>
2023-07-13 17:35:33 -0400

    working on public pages

--- chat_client/src/App.jsx
+++ chat_client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 import { ChatEngine } from 'react-chat-engine';
 import axios from 'axios'
+import loading from './assets/loading.gif';
 
 function App() {
   const [theUser, setTheUser] = useState()
@@ -10,19 +11,22 @@ function App() {
   const secret = 'BeeDevServices'
   const proj = '34e251b1-74c5-4888-b1b1-e56e45673e6e'
 
-  useEffect(() => {
-    window.addEventListener('message', function(event) {
-      console.log('i am in event listener function inside useEffect')
-      const userId = event.data;
-      console.log('event.data = userId', userId)
-      setTheName(userId)
-      setTheSecret(userId+secret)
-    })
-  },[])
-  console.log('after useeffect', theName)
+  // useEffect(() => {
+  //   window.addEventListener('message', function(event) {
+  //     console.log('i am in event listener function inside useEffect')
+  //     const userId = event.data;
+  //     console.log('event.data = userId', userId)
+  //     setTheName(userId)
+  //     setTheSecret(userId+secret)
+  //   })
+  // },[])
+  // console.log('after useeffect', theName)
 
   if (!theName || !theSecret) {
-    return <div>Loading...</div>; // or any loading indicator
+    return <div>
+      <h1>Logging you into Chat</h1>
+      <img src={loading} alt="Loading" />
+      </div>; // or any loading indicator
   }
 
   return (
