fddc3ddcc9e0a2089a6976086ca8e4a13530f56b
Melissa Longenberger <melissalongenberger24@gmail.com>
2023-08-17 18:24:04 -0400

    added json user count to footer pusing right to dev and main

--- coreApp/urls.py
+++ coreApp/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('api/', views.apiBase),
     path('api/allFoodData/', views.allFoodData),
     path('api/foodData/<str:cat>/', views.foodData),
+    path('api/userCount/', views.userCount),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
