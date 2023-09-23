31c9b044b24d9780b2dbd897e10b753a934c8dcb
Melissa Longenberger <melissalongenberger24@gmail.com>
2023-09-22 20:05:44 -0400

    saving chat model added not migrated

--- chatApp/models.py
+++ chatApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from userApp.models import *
 
-# Create your models here.
+class Chat(models.Model):
+    title = models.CharField(max_length=255)
+    adminUser = models.CharField(max_length=255)
+    chatID = models.IntegerField()
+
+class ChatMembers(models.Model):
+    chatUser = models.ForeignKey(User, related_name='theChatUser', on_delete=CASCADE)
+    chat = models.ForeignKey(Chat, related_name='theChat', on_delete=CASCADE)
