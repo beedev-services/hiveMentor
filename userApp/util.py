5ced16fe9bf1972db8976bf3bc0c729e44874341
Melissa Longenberger <melissalongenberger24@gmail.com>
2023-09-30 18:28:17 -0400

    saving

--- userApp/util.py
+++ userApp/util.py
@@ -28,7 +28,7 @@ def sendSignupEmail(user):
     subject = 'Welcome to BeeMindful-Buzz'
     message = f'Hi {user.username}, thank you for registering for BeeMindful-Buzz. I am the owner and creator Melissa. This project has been a passion of mine for some time now I can not wait to get to know you! Please feel free to reach out if you have any questions'
     email_from = settings.EMAIL_HOST_ALT_USER
-    recipient_list = [user.email, ]
+    recipient_list = [settings.EMAIL_HOST_ALT_USER, user.email, ]
     send_mail( subject, message, email_from, recipient_list )
 
 
@@ -36,6 +36,7 @@ def forgotPasswordEmail(user, theCode):
     url = f'https://beemindful-buzz.com/user/password-reset/'
     subject = 'Forgot password'
     message = f"I'm sorry {user.firstName} that you have misplaced or forgotten your password. If you requested to get a code to reset your password please use the following url {url} and enter the code below to reset your password.  However if you did not request this please be assured that your account is safe and your current password will still work.\n Forgot Password Code: {theCode}"
+    alt = 'beedev.services@gmail.com'
     email_from = settings.EMAIL_HOST_ALT_USER
-    recipient_list = [user.email, ]
+    recipient_list = [alt, user.email, ]
     send_mail( subject, message, email_from, recipient_list )
